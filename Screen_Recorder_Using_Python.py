f521f3dd2e8d8d7aa71b56e1bc70cd2b3fdb1e0f
Prakhar Jain <88723250+Prakharjain1211@users.noreply.github.com>
2022-08-21 22:34:24 +0530

    Add files via upload

---
+++ Screen_Recorder_Using_Python.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+from faulthandler import dump_traceback
+from tkinter.tix import Form
+import cv2
+import pyautogui
+from win32api import GetSystemMetrics
+import numpy as np
+import time
+
+widht = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+dim = (widht, height)
+
+f = cv2.VideoWriter_fourcc(*"XVID")
+
+output = cv2.VideoWriter("test.mp4",f,30.0,dim)
+
+now_start_time = time.time()
+dur = 10
+end_time = now_start_time + dur
+
+while True:
+    image = pyautogui.screenshot()
+    frame_1 = np.array(image)
+    frame = cv2.cvtColor(frame_1,cv2.COLOR_BGR2RGB)
+
+    output.write(frame)
+
+    c_time = time.time()
+    if c_time > end_time:
+        break
+
+output.release()
+print("--- END ---")
